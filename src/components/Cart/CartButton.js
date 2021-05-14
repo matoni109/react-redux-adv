44165f5a5ac1a726e8dfd2be8e4ee47d3751831c
Chris Wade <chris@wadespace.net>
2021-05-14 15:55:09 +1000

    update

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -3,16 +3,21 @@ import { useSelector } from "react-redux";
 
 const CartButton = (props) => {
   const cartItems = useSelector((state) => state.cartItems.cartItems);
+  const showCart = useSelector((state) => state.ui);
 
   const items = cartItems
     .map((item) => item.quantity)
     .reduce((a, b) => a + b, 0);
 
   return (
-    <button className={classes.button}>
-      <span>My Cart</span>
-      <span className={classes.badge}>{items}</span>
-    </button>
+    <>
+      {items > 0 && (
+        <button className={classes.button}>
+          <span>My Cart</span>
+          <span className={classes.badge}>{items}</span>
+        </button>
+      )}
+    </>
   );
 };
 
