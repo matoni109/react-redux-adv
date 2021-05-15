9af423df9e57b4727f52e42592d7104755cb86de
Chris Wade <chris@wadespace.net>
2021-05-15 18:43:04 +1000

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { useContext, useEffect } from "react";
+import { useSelector } from "react-redux";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
 
 function App() {
+  const cartItems = useSelector((state) => state.cartItems.cartItems);
+  console.log(cartItems);
+  // if cart changes below fires
+  useEffect(() => {
+    console.log("sendme");
+    fetch(
+      "https://react-db-api-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+      {
+        method: "PUT",
+        dataType: "json",
+        body: JSON.stringify(cartItems),
+      }
+      // PUT overides existing data
+    );
+    // .then((res) => res.json())
+    // .then((data) => console.log(data));
+  }, [cartItems]);
+
   return (
     <Layout>
       <Cart />
